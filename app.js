c37298ccb5a7b3f208d573a89e4dd20a3ae9f66e
manzeelanepali <nepalimanzeela@gmail.com>
2023-05-28 16:31:24 +0545

    feat : code till the immersion of the chicken group from the top of the game container

--- app.js
+++ app.js
@@ -6,17 +6,27 @@ document.addEventListener("DOMContentLoaded", () => {
     const mainPlayer = document.querySelector(".main-player-bird")
   
     let playerPositionX = 500;
+    // let currentOpposite = false ;
+    const chickeHeight =40;
+   
+    const chickenGap =20;
+    // let intervalId;
     function startGame() {
-        // console.log("hey");
+      
         instructionContainer.style.display = "none";
         mainContainer.style.display = "block";
         showMainPlayer()
         setupKeyboardControls();
-         setInterval( generateChicken, 2000); 
+
+        // for (let i=0;i<4;i++){
+            
+        // }
+        setInterval( generateChickenGroup(), 1000); 
+     
     }
 
     function showMainPlayer(){
-        console.log("check")
+       
         mainPlayer.style.display="block"
         
     }
@@ -30,36 +40,70 @@ document.addEventListener("DOMContentLoaded", () => {
         if (key === "ArrowLeft") {
             // Move player to the left
             playerPositionX -= 10; // Adjust the value according to your desired movement speed
+            if(playerPositionX<0)playerPositionX=0;
+           
         } else if (key === "ArrowRight") {
             // Move player to the right
             playerPositionX += 10; // Adjust the value according to your desired movement speed
+             
+               if(playerPositionX>960)playerPositionX=960;
         }
         
         // Update the player's position
         mainPlayer.style.left = playerPositionX + "px";
     }
 
-  function generateChicken() {
-    const enemyChicken = document.createElement("div");
-    enemyChicken.classList.add("enemy-chicken");
 
-    const randomPositionX = Math.floor(Math.random() * (gameContainer.offsetWidth - 50));
-    enemyChicken.style.left = randomPositionX + "px";
-    enemyChicken.style.top = "0px"; // Place the enemy chicken at the top side of the game container
 
-    gameContainer.appendChild(enemyChicken);
+function generateChickenGroup() {
+  const enemyGroup = document.createElement("div");
+  enemyGroup.classList.add("enemy-group");
 
-    const fallInterval = setInterval(() => {
-      const currentTop = parseInt(enemyChicken.style.top);
-      const newTop = currentTop + 5; // Adjust the value to control the falling speed
+  const numRows = 4; // Number of rows of enemy chickens
+  const numChickensPerRow = 16; // Number of enemy chickens in each row
+ 
+  
+  
 
-      if (newTop >= gameContainer.offsetHeight) {
-        clearInterval(fallInterval);
-        enemyChicken.remove();
-      } else {
-        enemyChicken.style.top = newTop + "px";
+  for(let j = 0 ; j< numRows ;j++){
+
+const chickenRow = document.createElement("div");
+      chickenRow.classList.add("enemy-row");
+
+  for (let i = 0; i < numChickensPerRow; i++) {
+        const enemyChicken = document.createElement("img");
+        enemyChicken.src = "hen.gif"; // Replace "hen.gif" with the path to your chicken image
+        enemyChicken.classList.add("enemy-chicken");
+
+        chickenRow.appendChild(enemyChicken);
       }
-    }, 50); // Adjust the interval to control the falling speed
+
+
+      enemyGroup.appendChild(chickenRow);
   }
+
+
+
+ 
+
+enemyGroup.style.top= - (numRows*( chickeHeight+chickenGap)-chickenGap) + "px"
+
+   
+
+
+ setInterval( ()=>{let currentTop = parseInt(enemyGroup.offsetTop)
+ 
+   let newTop = currentTop + 5
+   enemyGroup.style.top = newTop+ "px"},500
+
+ )
+ 
+
+
+  gameContainer.appendChild(enemyGroup);
+}
+
+
+
     startButton.addEventListener('click', startGame);
 });
